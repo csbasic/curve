32f91a2790ba6aa0187c70c703e6777cdcfcec5e
Chi Serge <chijokae@gmail.com>
2024-05-06 11:57:47 +0100

    Contact model and for method created in Contact controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
@@ -10,5 +11,20 @@ class ContactController extends Controller
     {
         $subtitle = 'Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit';
         return view("contact.index", ['page' => 'Contact', 'subtitle' => $subtitle]);
+    }
+
+    public function store(Request $request)
+    {
+
+        $formfield = $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'subject' => 'required',
+            'message' => 'required'
+        ]);
+
+        Contact::create($formfield);
+
+        return back()->with('message', 'Message sent successfully!');
     }
 }
